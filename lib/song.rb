b0f17f962628722de60b4bc547f1e03c71e91cde
Adam Priest <sepone229@gmail.com>
2020-03-22 02:42:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,6 @@ class Song
   attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
-    @name, @artist, @genre = name, artist, genre
     @@count += 1
     @@genre << genre
     @@artist << artist
