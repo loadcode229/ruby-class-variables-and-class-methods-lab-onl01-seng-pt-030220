0c23e52111fce5b3f1925b0decde08664aa79f6a
Adam Priest <sepone229@gmail.com>
2020-03-22 02:05:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,16 @@ class Song
   def initialize(name, artist, genre)
     @name, @artist, @genre = name, artist, genre
     @@count += 1
+    @@genres += 1
   end
   
   def self.count
     return @@count
   end
+  
+  def self.genres
+    @@genres = []
+    
+    
   
 end
